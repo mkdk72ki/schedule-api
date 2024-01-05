d87f27a108657ed9c9084b63eed82990bb01d47a
mkdk72ki <harukazexsysy@gmail.com>
2024-01-06 00:23:53 +0900

    SQL文の修正、findById()の追加

--- src/main/java/com/mkdk/schedule/ScheduleMapper.java
+++ src/main/java/com/mkdk/schedule/ScheduleMapper.java
@@ -8,17 +8,21 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface ScheduleMapper {
   @Select("SELECT * FROM schedule")
   List<Schedule> findAll();
 
+  @Select("SELECT * FROM schedule WHERE schedule_id = #{scheduleId}")
+  Optional<Schedule> findById(Integer scheduleId);
+
   @Insert("INSERT INTO schedule (schedule_id, user_id, group_id, title, schedule_date, start_time, end_time, comment) VALUES (#{scheduleId}, #{userId}, #{groupId}, #{title}, #{scheduleDate}, #{startTime}, #{endTime}, #{comment})")
   @Options(useGeneratedKeys = true, keyProperty = "scheduleId")
   void create(Schedule schedule);
 
-  @Update("UPDATE schedule SET group_id = #{groupId}, title = #{title}, schedule_date = #{scheduleDate}, start_time = #{startTime}, end_time = #{endTime}, comment = #{comment}")
+  @Update("UPDATE schedule SET group_id = #{groupId}, title = #{title}, schedule_date = #{scheduleDate}, start_time = #{startTime}, end_time = #{endTime}, comment = #{comment} WHERE schedule_id = #{scheduleId}")
   void update(Schedule schedule);
 
   @Delete("DELETE FROM schedule WHERE schedule_id = #{scheduleId}")
