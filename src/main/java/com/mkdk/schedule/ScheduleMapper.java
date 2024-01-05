d03875b8c9bfb1a157b0bb4365aa006478742075
mkdk72ki <harukazexsysy@gmail.com>
2024-01-05 22:58:42 +0900

    SQL文を修正

--- src/main/java/com/mkdk/schedule/ScheduleMapper.java
+++ src/main/java/com/mkdk/schedule/ScheduleMapper.java
@@ -14,14 +14,14 @@ public interface ScheduleMapper {
   @Select("SELECT * FROM schedule")
   List<Schedule> findAll();
 
-  @Insert("INSERT INTO schedule (schedule_id, user_id, group_id, title, schedule_date, start_time, end_time, comment) VALUES (#{schedule_id}, #{user_id}, #{group_id}, #{title}, #{schedule_date}, #{start_time}, #{end_time}, #{comment})")
-  @Options(useGeneratedKeys = true, keyProperty = "id")
+  @Insert("INSERT INTO schedule (schedule_id, user_id, group_id, title, schedule_date, start_time, end_time, comment) VALUES (#{scheduleId}, #{userId}, #{groupId}, #{title}, #{scheduleDate}, #{startTime}, #{endTime}, #{comment})")
+  @Options(useGeneratedKeys = true, keyProperty = "scheduleId")
   void create(Schedule schedule);
 
-  @Update("UPDATE schedule SET group_id = #{group_id}, title = #{title}, schedule_date = #{schedule_date}, start_time = #{start_time}, end_time = #{end_time}, comment = #{comment}")
+  @Update("UPDATE schedule SET group_id = #{groupId}, title = #{title}, schedule_date = #{scheduleDate}, start_time = #{startTime}, end_time = #{endTime}, comment = #{comment}")
   void update(Schedule schedule);
 
-  @Delete("DELETE FROM schedule WHERE schedule_id = #{schedule_id}")
-  void delete(int id);
+  @Delete("DELETE FROM schedule WHERE schedule_id = #{scheduleId}")
+  void delete(Integer scheduleId);
 
 }
