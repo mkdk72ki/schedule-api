69339e4556ab52658c50975da019015f3963fd95
mkdk72ki <harukazexsysy@gmail.com>
2024-01-15 22:46:41 +0900

    バリデーションの追加

--- src/main/java/com/mkdk/schedule/controller/form/GroupUpdateForm.java
+++ src/main/java/com/mkdk/schedule/controller/form/GroupUpdateForm.java
@@ -1,5 +1,6 @@
 package com.mkdk.schedule.controller.form;
 
+import com.mkdk.schedule.validation.UniqueCode;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 
@@ -9,6 +10,8 @@ public class GroupUpdateForm {
   private String groupName;
 
   @Length(min = 5, max = 20, message = "5文字以上20字以内で入力してください")
+  @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "半角英数字のみで入力してください")
+  @UniqueCode
   private String groupCode;
 
   @Length(min = 5, max = 30, message = "5字以上30字以内で入力してください")
